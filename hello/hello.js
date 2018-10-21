4e78e256b1ec5ca389df2ad4561d5767506ad10d
xzx <xuzhenxing@canaan-creative.com>
2018-10-21 18:46:27 +0800

    Add main() for hello.js

--- hello/hello.js
+++ hello/hello.js
@@ -1,8 +1,12 @@
-// Showing to Web
-// Line signal: <br> in document.write
-document.write("Hello JavaScript! <br>  Hello Github!<br>");
+function main() {
+    // Showing to Web
+    // Line signal: <br> in document.write
+    document.write("Hello JavaScript! <br>  Hello Github!<br>");
 
-// Browser message
-document.write("IE's Name: " + navigator.appName+"<br>");
-document.write("IE's Version: " + navigator.appVersion+"<br>");
-document.write("IE's release id: " + navigator.appCodeName+"<br>");
+    // Browser message
+    document.write("IE's Name: " + navigator.appName+"<br>");
+    document.write("IE's Version: " + navigator.appVersion+"<br>");
+    document.write("IE's release id: " + navigator.appCodeName+"<br>");
+}
+
+main()
