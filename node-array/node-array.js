dd0ab93982372f55028afe9dcbacb27db92ebaf2
xuzhenxing <xzxlnmail@163.com>
2019-01-06 16:36:11 +0800

    Add node arrays

---
+++ node-array/node-array.js
@@ -0,0 +1,6 @@
+function main() {
+    document.querySelector("#xdl").innerHTML = "My new id is a good habit";
+    document.querySelectorAll(".xdh")[0].innerHTML = "My new id is loving javascript";
+}
+
+main()
